b3e45b36d0983fee0340688dc5f12403855082bf
Paulo Lourenço <paulohenrilourenco@gmail.com>
2018-05-13 15:10:57 -0300

    template dashboard configurado com backend

--- angular1/gulpTaks/server.js
+++ angular1/gulpTaks/server.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const watch = require('gulp-watch')
-const weserver = require('gulp-webserver')
+const webserver = require('gulp-webserver')
 
 gulp.task('watch', () => {
     watch('app/**/*.html', () => gulp.start('app.html'))
